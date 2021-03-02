6384703f7bc14ceb6fe1f64d263267a1cf7354b4
GuilhermeAngel3 <guilherme.angeloni@aluno.unifafibe.edu.br>
2021-03-02 23:29:19 +0000

    Tarefa atualizada mk2

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Olá tudo bem? Eu sou Guilherme Simieli Angeloni e moro em Bebedouro-SP.");
+    System.out.println("Ola, tudo bem? Eu sou Guilherme Simieli Angeloni e moro em Bebedouro-SP.");
   }
 }
